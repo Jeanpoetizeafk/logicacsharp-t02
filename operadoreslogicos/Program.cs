de80c10b5197443ef414080e033c423aac63e4f4
JEAN LUCCA GARDIANO DOS SANTOS <jean.lgsantos@senacsp.edu.br>
2025-11-06 15:02:26 -0300

    atualizando

---
+++ operadoreslogicos/Program.cs
@@ -0,0 +1,13 @@
+﻿// Operadores logicos
+bool x = true;
+bool y = false;
+
+bool and = x && y; // e logico
+bool or = || y; // ou logico
+bool not = !x; // nao logico
+
+// Exibindo resultado dos operadores logicos
+Console.WriteLine("\noperadores logicos:");
+Console.WriteLine($"x && y: {and}");  //
+Console.WriteLine($"x || y: {or}");  //
+Console.WriteLine($"!x: {not}");   //
---
+++ operadoresrelacionais/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+int a = 10;
+int b = 5;
+
+//Operadores Relacionais
+bool maiorque = a > b;
+bool menorque = a < b;
+bool igual = a == b;
+bool diferente = a != b;
+bool maiorouigual = a >= b;
+bool menorouigual = a <= b;
+
+// exibindo resultado dos operadores relacionais 
+Console.WriteLine("\noperadores Relacionais:");
+Console.WriteLine($"{a} > {b}: {maiorque}");
+Console.WriteLine($"{a} == {b}: {igual}");
+Console.WriteLine($"{a} != {b}: {diferente}");
+Console.WriteLine($"{a} >= {b}: {maiorouigual}");
+Console.WriteLine($"{a} <= {b}: {menorouigual}");
