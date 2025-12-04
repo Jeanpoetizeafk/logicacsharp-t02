23628b04ef61bb1f5ecc8e137b068e1b05f2060c
JEAN LUCCA GARDIANO DOS SANTOS <jean.lgsantos@senacsp.edu.br>
2025-12-04 16:49:48 -0300

    aula04/12

---
+++ Listas/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Progran
+{
+
+    static void Main()
+    {
+
+        List<string> nomes = new List<string>();
+
+        // adicionando elementos
+        nomes.Add("Ana");
+        nomes.Add("Bruno");
+        nomes.Add("Carlos");
+
+        Console.WriteLine(nomes[0]); //Ana
+        Console.WriteLine(nomes[1]); //Bruno
+        Console.WriteLine(nomes[2]); //Carlos
+
+
+
+        Console.WriteLine("lista apos adicionar elementos:");
+        foreach ( String nome in nomes )
+        {
+            Console.WriteLine(nome);
+        }
+
+        //adiciona novo nome
+        nomes.Add("Daniel");
+
+        Console.WriteLine("\nLista apos adicionar 'Daniel':");
+        foreach (string nome in nomes)
+        {
+            Console.WriteLine(nome);
+        }
+
+        //Removendo elementos
+        nomes.Remove("Bruno"); // Remove pelo valor
+
+        Console.WriteLine("\nLista apos remover 'Bruno':");
+        foreach ( String nome in nomes )
+        {
+            Console.WriteLine(nome);
+        }
+
+        // Removendo pelo indice
+        nomes.RemoveAt(0); //Remove o primeiro (Ana)
+
+        Console.WriteLine("\nLista apos remover o indice 0 (Ana):");
+        foreach( String nome in nomes )
+        {
+            Console.WriteLine(nome);
+        }
+
+        // Limpando toda a lista
+        nomes.Clear();
+
+        Console.WriteLine("\nLista apos Clear():");
+        Console.WriteLine("Quantidade de elementos:" + nomes.Count);
+
+
+    }
+
+}
---
+++ classe com heranca/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica14
+{
+    //Classe base (superclasse)
+    internal class Animal
+    {
+        //Propriedade de classe animal
+        public string Nome { get; set; } //atributo público do tipo string
+
+        //Método que será sobrecarregado nas classes derivadas
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine($"O animal {Nome} emite um som.");
+        }
+    }
+    // Classe derivada (subclasse) - "herança"
+    internal class Cachorro : Animal
+    {
+        //Sobrescrevendoo método "EmitirSom"
+        public override void EmitirSom()
+        {
+            Console.WriteLine($"{Nome} late: Au Au!");
+        }
+    }
+
+    //Outra Classe derivada
+    internal class Gato : Animal
+    {
+        //sobescrevendo o metodo EmitirSom
+        public override void EmitirSom()
+        {
+            Console.WriteLine($"{Nome} mia: Miau!");
+        }
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando instancias das classes derivadas
+            Animal meuCachorro = new Cachorro { Nome = "rex" };
+            Animal meuGato = new Gato { Nome = "felix" };
+            Animal meuCoelho = new Animal { Nome = "Lulu" };
+
+            //Chamando o metodo EmitirSom para cada animal
+            meuCachorro.EmitirSom(); //Saida: Rex Lata: Au Au!
+            meuGato.EmitirSom();// Saida: Felix mia: Miau!
+
+            // Utilizando Polimorfismo: Array de animais
+            Animal[] animeis = { meuCachorro, meuGato, meuCoelho };
+
+                foreach (var animal in animeis)
+            {
+                animal.EmitirSom(); // Cada objeto chama o metodo adequado de sua classe
+
+            }
+        }
+    }
+
+}
